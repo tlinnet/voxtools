5a74739a07290108f1aade186a05c0b2b78d3288
Troels Schwarz-Linnet <tlinnet@gmail.com>
2018-01-24 21:35:30 +0100

    Initial check-in

--- README.md
+++
@@ -1,2 +0,0 @@
-# voxtools
-VoxTools
---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from codecs import open
+from os import path
+from voxtools import __version__
+
+# Determine install position
+package_name = 'voxtools'
+import sys
+from site import USER_SITE, USER_BASE
+rela_path = USER_SITE.split(USER_BASE)[-1][1:]
+install_to = path.join(rela_path, package_name)
+print(rela_path)
+print(install_to)
+
+# get long description from README
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name=package_name,
+    version=__version__,
+    description='A module for working with voxtools.',
+    long_description=long_description,
+    url='https://github.com/tlinnet/voxtools',
+    download_url='https://github.com/tlinnet/voxtools/archive/%s.tar.gz'%__version__,
+    author='Troels Schwarz-Linnet',
+    author_email='tlinnet@gmail.com',
+    license='Apache Software License',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux'],
+    requires=['numpy', 'openpyxl', 'PyQt5'],
+    packages=['voxtools'],
+    data_files=[(install_to, ['README.rst', 'LICENSE'])],
+)
