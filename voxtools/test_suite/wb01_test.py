f865b07051ab5100f19e796337246b0afb11fd38
Troels Schwarz-Linnet <tlinnet@gmail.com>
2018-01-27 16:44:46 +0100

    Adding test for wb 01

--- voxtools/test_suite/wb01_test.py
+++ voxtools/test_suite/wb01_test.py
@@ -10,9 +10,11 @@
 ################################################################################
 
 import datetime, os, os.path, tempfile, unittest
+from openpyxl.utils import get_column_letter
 
 # Import voxtools excel
 from voxtools import excel
+
 
 class Test_wb01(unittest.TestCase):
     def setUp(self):
@@ -28,17 +30,35 @@ class Test_wb01(unittest.TestCase):
         self.excel_src = os.path.join(shared_data_dir, 'wb01.xlsx')
 
         # Instantiate the Excel class
-        exl = excel.excel(excel_src=self.excel_src, excel_dst=self.excel_dst)
+        self.exl = excel.excel(excel_src=self.excel_src, excel_dst=self.excel_dst)
         # Run it
-        exl.run_all()
+        self.exl.run_all()
+
 
     def test_wb(self):
-        print(self.excel_src)
-        print(self.excel_dst)
+        # Assert the sheetnames
+        self.assertEqual(self.exl.wb.sheetnames, ['Ark1', 'Køn'])
+
+        # Get sheetname
+        ws = self.exl.wb['Køn']
+
+        # Assert number of rows and cols
+        self.assertEqual(ws.max_row, 18)
+        self.assertEqual(ws.max_column, 9)
+        self.assertEqual(get_column_letter(ws.max_column), "I")
+
+        # Assert the width
+        self.assertEqual(ws.column_dimensions['A'].width, 40+0.83)
+
+        # Assert the height
+        self.assertEqual(ws.row_dimensions[18].height, 15)
+
 
     def tearDown(self):
         # Delete temporary file
+        print("Deleting : %s"%self.excel_dst)
         os.remove(self.excel_dst)
+
 
 if __name__ == '__main__':
     unittest.main()
